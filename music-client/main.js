af98977bcfb341a2a3b968f4a71881d674eb27b5
Nebeyu Z <64758297+Nebeyu2001@users.noreply.github.com>
2022-05-18 09:46:12 -0500

    "final"

--- music-client/main.js
+++ music-client/main.js
@@ -123,8 +123,7 @@ function fetchMusic() {
                         <th scope="row">${counter}</th>
                         <td>${song.title}</td>
                          <td>${song.releaseDate}</td>
-                         <td>
-                         <input type="button" musicid="${song.id}" onclick="addToMyPlayList(this)" value="ADD"/>
+                        <td> <input type="button" musicid="${song.id}" onclick="addToMyPlayList(this)" value="ADD"/></td>
                          
 
                      </tr>               
@@ -188,8 +187,6 @@ function fetchPlayList() {
     );
 }
 function addToMyPlayList(song) {
-  //songId = "3854de5d-aa34-431e-b3f5-d924e04a4f26";
-
   let songAtt = song.getAttribute("musicid");
   console.log(songAtt);
 
@@ -223,7 +220,7 @@ function addToMyPlayList(song) {
                     <tr>
                         <td scope="row">${song.orderId}</td>
                         <td>${song.title}</td>
-                        <td><input type="button" playlistId="${song.songId}"  onclick="removePlayList(this)"  value="remove"/><input type="button" playSong="${song.urlPath}" onclick="playSong(this)" value="play"/></td>
+                        <td><input type="button" playlistId="${song.songId}"  onclick="removePlayList(this)"  value="remove"/>&nbsp&nbsp<input type="button" playSong="${song.urlPath}" onclick="playSong(this)" value="play"/></td>
                      </tr>
                `;
       });
@@ -274,7 +271,7 @@ function removePlayList(song) {
         <tr>
           <td scope="row">${song.orderId}</td>
           <td>${song.title}</td>
-          <td><input type="button" playlistId="${song.songId}" onclick="removePlayList(this)"  value="remove"/><input type="button" playSong="${song.urlPath}" onclick="playSong(this)"  value="play"/></td>
+          <td><input type="button" playlistId="${song.songId}" onclick="removePlayList(this)"  value="remove"/>&nbsp&nbsp<input type="button" playSong="${song.urlPath}" onclick="playSong(this)"  value="play"/></td>
        </tr>
                  `;
         });
@@ -300,35 +297,13 @@ function playSong(song) {
   x.play();
 }
 
-// function playSong(song) {
-//   let playAtt = song.getAttribute("playSong");
-
-//   fetch(`${SERVER_ROOT}/${playAtt}`, {
-//     headers: {
-//       Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-//       "Content-Type": "application/json",
-//     },
-//   })
-//     .then((response) => response.json())
-//     .then((songs) => {
-//       let searchSong = document.getElementById("footer-player");
-//       searchSong.innerHTML = `<audio controls class="embed-responsive-item">
-//   <source src="${SERVER_ROOT}/${playAtt} "type ="audio/mpeg"/>
-// </audio>`;
-// }
-
 function afterLogin() {
-  // <audio controls>
-  // <source src="${SERVER_ROOT}/api/${song.urlPath}" type="audio/mp3">
-  // </audio>
-
   document.getElementById("search").style.display = "block";
   document.getElementById("logout-div").style.display = "block";
   document.getElementById("login-div").style.display = "none";
   fetchMusic();
   fetchPlayList();
   document.getElementById("content").innerHTML = "Song Options";
-  //document.getElementById("mTable").style.display = "block-inline";
 }
 
 function notLogin() {
